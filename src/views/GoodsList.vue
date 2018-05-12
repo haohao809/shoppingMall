56c9463f0b646b4031f811602b8579e224cf6143
haohao809 <haohao809@126.com>
2018-05-12 22:37:29 +0800

    css

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,7 +1,26 @@
 <template>
+	<div id="">
+		<head></head>
+		<div id="">
+			
+		</div>
+		<div id="">
+			
+		</div>
+		<footer></footer>
+	</div>
 </template>
 
 <script>
+	import './../assets/css/base.css'
+	import './../assets/css/product.css'
+	export default{
+		data() {
+			return {
+				
+			}
+		}
+	}
 </script>
 
 <style>
