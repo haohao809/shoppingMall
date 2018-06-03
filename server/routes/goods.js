6d7c4c383008b6fc223f6279634b72283e962457
haohao809 <haohao809@126.com>
2018-06-03 16:54:40 +0800

    mongoDB

---
+++ server/routes/goods.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+
+var Goods = require('../models/goods');
+//连接mongoDB数据库
+mongoose.connect('mongodb://127.0.0.1:27017/anzhData');
+
+mongoose.connection.on("connected", function() {
+	console.log("mongoDB connected success.")
+})
+mongoose.connection.on("error", function() {
+	console.log("mongoDB connected faile.")
+})
+mongoose.connection.on("disconnected", function() {
+	console.log("mongoDB connected disconnected.")
+})
+
+router.get("/",function(req,res,next) {
+	Goods.find({}, function(err,doc) {
+		if(err) {
+			res.json({
+				status: '1',
+				msg: err.message
+			})
+		}else{
+			res.json({
+				status:'0',
+				msg:'',
+				result:{
+					count:doc.length,
+					list:doc
+				}
+			})
+		}
+	})
+	res.send("hello,goods list .")
+})
+
+module.exports = router;
