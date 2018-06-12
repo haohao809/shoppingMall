8d192e081d2b372360547d958ac67d9dc1293988
haohao809 <haohao809@126.com>
2018-06-12 22:52:01 +0800

    login

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,4 +1,17 @@
 <template>
+	<header class="header">
+		<div class="navbar">
+			<div class="navbar-left-container">
+				<a href='/'>
+					<img class='navbar-brand-container' src="/static/logo.png">
+				</a>
+			</div>
+			<div class="navbar-brand-logo" style="display: flex;">
+				
+			</div>
+		</div>
+		
+	</header>
 </template>
 
 <script>
