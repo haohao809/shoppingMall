ca36d890119c3de57775eb9148b48693404b42db
haohao809 <haohao809@126.com>
2018-06-16 18:32:21 +0800

    login

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var ejs = require('ejs');
 var indexRouter = require('./routes/index');
@@ -18,6 +19,8 @@ app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
