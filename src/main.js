abc842245da1526e1344ecd1d40f2e370f086955
haohao809 <haohao809@126.com>
2018-07-11 22:20:51 +0800

    currency

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import App from './App'
 import router from './router'
 import VueLazyLoad from 'vue-lazyload'
 import infiniteScroll from  'vue-infinite-scroll'
-
+import {currency} from './util/currency'
 Vue.config.productionTip = false
 Vue.use(VueLazyLoad,{
 	loading:"/static/loading-svg/loading-bars.svg"
 })
 Vue.use(infiniteScroll);
+Vue.filter("currency",currency);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
